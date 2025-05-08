0abd41986085fc4303170e0b6cfb8c078432f79c
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-08 14:24:01 +0000

    Task No.5

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-# This prints the welcome message
-print("Welcome to {} {}!".format(str1, str2))
+str3 = str1 + " " + str2
+print("Welcome to " + str3 + "!")
