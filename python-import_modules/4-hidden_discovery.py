bfbbcf089c4e21c712da5e03d036348359efe018
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-10 19:04:27 +0000

    fix 4-hidden_discovery

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
 
     for name in sorted(dir(hidden_4)):
-        if not name.startswith("_"):
+        if not name.startswith("__"):
             print(name)
