5952b3bbdb01382a67483f6abec8b56d1f192dc1
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-28 04:07:08 +0000

    Roman to Integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    total = 0
+    prev = 0
+    for c in reversed(roman_string):
+        if roman_dict[c] < prev:
+            total -= roman_dict[c]
+        else:
+            total += roman_dict[c]
+        prev = roman_dict[c]
+    return total
