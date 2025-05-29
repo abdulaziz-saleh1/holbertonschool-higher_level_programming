c06e47d82cf6c1b0b5898eecfde662f1c96781ea
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-29 09:17:19 +0000

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,41 +1,27 @@
 #!/usr/bin/python3
 """
-Module prints new lines after chars..
+Function that prints a text with 2 new lines after each of
+these characters: ., ? and :
 """
-
 
 def text_indentation(text):
     """
-    Function prints 2 new lines after chars ., ? and :
+    Prints a text with 2 new lines after '.', '?', and ':'
 
     Args:
-        text(str):
+        text (str): The text to be printed.
 
-    Returns:
-
+    Raises:
+        TypeError: If text is not a string.
     """
     if not isinstance(text, str):
-        raise TypeError('text must be a string')
-    chars = '.?:'
+        raise TypeError("text must be a string")
 
-    if '.' not in text and '?' not in text and ':' not in text:  # check..
-        print(text, end="")
-    iter_index = 0
-    max_index = len(text) - 1
-    for index, value in enumerate(text):
-        if value in chars:
-            print(text[iter_index:index + 1])
-            print()
-            iter_index = index
-            if index < max_index and text[index + 1] == " ":
-                iter_index = index + 2
-                if '.' not in text[iter_index:] and ':' not in \
-                        text[iter_index:] and '?' not in text[iter_index:]:
-                    print(text[iter_index:len(text)], end="")
-                    break
-            if iter_index < max_index and text[index + 1] is not " ":
-                iter_index = index + 1
-                if '.' not in text[iter_index:] and ':' not in \
-                        text[iter_index:] and '?' not in text[iter_index:]:
-                    print(text[iter_index:len(text)], end="")
-                    break
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
+            while i + 1 < len(text) and text[i + 1] == " ":
+                i += 1
+        i += 1
