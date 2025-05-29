a2f24e348f46d0f6777b61a12ad0438e33f583b6
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-29 09:29:51 +0000

    Text indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This function takes text as an argument, loop through it and if
-it it read any of these characters ., ? and :, it will go to the next line
+it reads any of these characters ., ? and :, it will go to the next line
 """
 
 
@@ -9,23 +9,24 @@ def text_indentation(text):
     """
     a function that goes to newline after ., ? and :
 
-        Args:
-        Text: the text to be parised
+    Args:
+        text: the text to be parsed
 
     Returns:
         None
 
     Raises:
-        "TypeError": if text is not a string type
-
+        TypeError: if text is not a string type
     """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
+
     index = 0
     while index < len(text):
         print(text[index], end="")
         if text[index] in ".?:":
-            print("\n\n", end="")
-            while index + 1 < len(text) and text[index+1] == " ":
-                text = text[:index+1] + text[index+2:]
+            print()
+            print()
+            while index + 1 < len(text) and text[index + 1] == " ":
+                index += 1
         index += 1
