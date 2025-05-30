e86fe77381c1f848465f826330cda2aeb434f9f8
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-30 17:58:35 +0000

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -11,5 +11,6 @@ class Rectangle(BaseGeometry):
         """Initialize width and height as private attributes, with validation."""
         self.integer_validator("width", width)
         self.__width = width
+
         self.integer_validator("height", height)
         self.__height = height
