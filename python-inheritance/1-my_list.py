891cdfa67373551313c476cf021f6e2e3d9d48b8
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-30 17:23:36 +0000

    My list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,8 +2,9 @@
 """Module for MyList class."""
 
 class MyList(list):
-    """A subclass of list with a method to print it sorted."""
+    """A subclass of list with a method to print the list sorted."""
+
 
     def print_sorted(self):
-        """Prints the list sorted."""
+        """Prints the list in ascending sorted order."""
         print(sorted(self))
