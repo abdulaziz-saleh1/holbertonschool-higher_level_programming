db9e1c6bb7086feddcff12b0266d53eb8347d831
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-30 14:27:51 +0000

    Square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
- """ A class that defines a square."""
- 
+"""Defines a square class with a private instance attribute."""
+
+
 class Square:
+    """A class that defines a square by its size."""
+
     def __init__(self, size):
-        """ Initializes a square instance. """
+        """
+        Initializes a new Square instance.
+
+        Args:
+            size (int): The size of the square.
+        """
         self.__size = size
