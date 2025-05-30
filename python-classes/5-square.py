16bf3fdc1b45a96be6f6d44f5cd9906512a714ed
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-05-30 15:48:22 +0000

    Printing a square

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Defines a square class with size property, validation, area, and print method."""
+"""Defines a square class with size property,
+validation, area, and print method."""
 
 
 class Square:
