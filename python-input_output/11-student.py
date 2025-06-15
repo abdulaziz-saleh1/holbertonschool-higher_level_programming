4e910918e07e93f1218469ff960ab81e4cf977db
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-06-15 10:20:41 +0000

    Student to disk and reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Module that defines a Student class with support for JSON serialization
+and deserialization (reload from JSON).
+"""
+
+
+class Student:
+    """Defines a student by first name, last name, and age."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Returns a dictionary representation of the Student instance.
+        If attrs is a list of strings, only attributes in that list are
+        returned. Otherwise, all attributes are returned.
+        """
+        if isinstance(attrs, list) and all(
+            isinstance(attr, str) for attr in attrs
+        ):
+            return {
+                attr: getattr(self, attr)
+                for attr in attrs if hasattr(self, attr)
+            }
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student instance
+        with those in the given dictionary.
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
