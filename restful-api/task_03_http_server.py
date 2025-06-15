07bebe063620ec0589b3bc48662547f91961be2f
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-06-15 10:44:33 +0000

    Develop a simple API using Python

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""Simple HTTP API using http.server."""
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            data = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(data).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
+
+        elif self.path == "/info":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.wfile.write(json.dumps(info).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"404 Not Found: Endpoint not found.")
+
+
+def run():
+    server_address = ("", 8000)  # localhost:8000
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    print("Server running at http://localhost:8000")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
