c5397a80fa0da1cea0842bc2fe42963ff473a655
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-07-19 02:29:31 +0000

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python3
-"""Lists states matching the input from the database."""
+"""
+This script takes in 4 arguments (mysql username, password, database name,
+and state name searched) and lists all states starting with the given
+name (case-sensitive) from the database hbtn_0e_0_usa.
+"""
 
-import sys
 import MySQLdb
+import sys
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
-                         db=sys.argv[3], host="localhost", port=3306)
-    c = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}%' ORDER BY id ASC".format(sys.argv[4])
-    c.execute(query)
-    for state in c.fetchall():
-        print(state)
-    c.close()
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        host='localhost',
+        port=3306
+    )
+    cursor = db.cursor()
+    query = """
+        SELECT * FROM states
+        WHERE name LIKE BINARY '{}%'
+        ORDER BY id ASC
+    """.format(sys.argv[4])
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
     db.close()
