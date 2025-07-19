626bfeb928581461514682387f836c830dd104c4
abdulaziz-saleh1 <10710@holbertonstudents.com>
2025-07-19 01:48:16 +0000

    Filter states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Script that lists all states starting with N"""
+"""Lists states starting with N from the database."""
 
 import MySQLdb
-import sys
+from sys import argv
+
+
+def main():
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=argv[1], passwd=argv[2],
+                         db=argv[3], charset="utf8")
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE BINARY name LIKE 'N%' ORDER BY id ASC"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
+
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3]
-    )
-
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    for row in cur.fetchall():
-        print(row)
-
-    cur.close()
-    conn.close()
+    main()
